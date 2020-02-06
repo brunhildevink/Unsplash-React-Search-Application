11ecafe9c046c25edf224f3b9bebe8ec6e07641b
Brunhilde Vink <brunhilde@smartshore.nl>
2020-02-06 22:11:05 +0100

    Basic unsplash api call

--- unsplash-app/src/App.js
+++ unsplash-app/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Unsplash, { toJson } from "unsplash-js";
+import $ from 'jquery';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const fetch = require('node-fetch');
+    global.fetch = fetch;
+    let clientId = "2a1120b968b317bb1d590468bfda81a6584cc506329f39222f2698a19d189ac0";
+    let url = "https://api.unsplash.com/photos/?client_id=" + clientId;
+    fetch(url)
+
+    .then((response) => {
+      return response.json();
+    })
+    .then((myJson) => {
+
+      myJson.forEach(photo => {
+        console.log(photo)
+        let result = `<img src=${photo.urls.regular}/>`;
+        $("#result").append(result);
+      });
+    });
+
+    return (
+
+      <div className="App" id="result">
+      </div>
+    );
 }
 
 export default App;
